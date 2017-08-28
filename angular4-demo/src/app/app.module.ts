bc1e641736c5857878dea5979a704e7ff16642bf
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2017-08-28 10:26:28 +0400

    [(ngModel)] short version working.

--- angular4-demo/src/app/app.module.ts
+++ angular4-demo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [/* DataServiceService */],
   bootstrap: [AppComponent]
