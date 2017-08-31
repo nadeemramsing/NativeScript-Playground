ceecf219951dcdd5bb1e82a7d6dbeafd7cba9768
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2017-08-31 11:11:05 +0400

    Using ActivatedRoute working (coms bet. comps).

--- mytodolist/src/app/components/form/form.component.ts
+++ mytodolist/src/app/components/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { SharedService } from './../../services/shared.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -7,19 +9,34 @@ import { SharedService } from './../../services/shared.service';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+
   protected title: String;
   protected description: String;
   protected date: Date;
   protected isDone: Boolean;
 
-  constructor(private sharedService: SharedService) { }
+  constructor(private sharedService: SharedService, private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.sharedService.currentTask.subscribe(res => {
+      //Using SharedService, Observable and Subject
       this.title = res.title;
       this.description = res.desc;
       this.date = res.date;
       this.isDone = res.isDone;
+
+      //Using queryParams
+      this.activatedRoute.queryParams.subscribe(params => {
+        var task = JSON.parse(params.task);
+
+        this.title = task.title;
+        this.description = task.desc;
+        this.date = task.date;
+        this.isDone = task.isDone;
+      });
+
+      //Using routeParams (not implemented)
+      /* this.activatedRoute.params.subscribe(task => console.log(task)); */
     });
   }
 
