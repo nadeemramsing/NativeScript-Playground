cb23761c53fffccc2d7401ca9a5f2474abcf88c6
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2017-09-06 16:10:37 +0400

    Event binding tested and working.

--- groceries/app/app.component.ts
+++ groceries/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template:
+  /* The res:// syntax tells NativeScript to use a platform-specific resource, in this case an image. Platform-specific resources go in your app's app/App_Resources folder. */
   `
   <FlexboxLayout flexDirection="column" justifyContent="space-around">
     <TextField hint="Email Address" keyboardType="email"
     autocorrect="false" autocapitalizationType="none"></TextField>
     <TextField hint="Password" secure="true"></TextField>
     
-    <Button text="Sign in" class="submit-button"></Button>
+    <Button text="Sign in" class="submit-button" (tap)="submit()"></Button>
     <Button text="Sign up for Groceries"></Button>
     <Image alignSelf="center" src="res://logo_login" stretch="none"></Image>
     
@@ -18,4 +19,11 @@ import { Component } from "@angular/core";
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"] /* Component-specific styling */
   /* REMEMBER: login.android.css -> login.css (Under Android folder) */
 })
-export class AppComponent { }
+export class AppComponent {
+
+  constructor(){}
+
+  public submit() {
+    console.log("submitted");
+  }
+}
