9f763eff5f63dc5100b64a2ed17158aab7d195dd
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2017-09-06 16:35:27 +0400

    templateUrl used instead of template.

--- groceries/app/app.component.ts
+++ groceries/app/app.component.ts
@@ -3,20 +3,7 @@ import { User } from "./shared/user/user";
 
 @Component({
   selector: "my-app",
-  template:
-  /* The res:// syntax tells NativeScript to use a platform-specific resource, in this case an image. Platform-specific resources go in your app's app/App_Resources folder. */
-  `
-  <FlexboxLayout flexDirection="column" justifyContent="space-around">
-    <TextField hint="Email Address" keyboardType="email" [(ngModel)]="user.email"
-    autocorrect="false" autocapitalizationType="none"></TextField>
-    <TextField hint="Password" secure="true"></TextField>
-    
-    <Button [text]="isLoggingIn ? 'Sign in' : 'Sign up'" class="submit-button" (tap)="submit()"></Button>
-    <Button [text]="isLoggingIn ? 'Sign up' : 'Back to login'" (tap)="toggleDisplay()"></Button>
-    <Image alignSelf="center" src="res://logo_login" stretch="none"></Image>
-    
-  </FlexboxLayout>
-`,
+  templateUrl: 'pages/login/login.html',
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"] /* Component-specific styling */
   /* REMEMBER: login.android.css -> login.css (Under Android folder) */
 })
